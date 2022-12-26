6a5108fc097873ee291c2eb61e731c2189795a04
Stepan Semenov <ishee11@gmail.com>
2022-12-26 18:50:18 +1000

    version for server

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ TOKEN = "5871071094:AAFxcN4mTaEfUlrQ_cRvmdougqygEj05uUI" #paleo
 admin_chat_id = "287689713" #ID администратора
 
 # webhook settings
-WEBHOOK_HOST = f'https://95.163.235.27'
+WEBHOOK_HOST = f'95-163-235-27.cloudvps.regruhosting.ru'
 WEBHOOK_PATH = f'/webhook/{TOKEN}'
 WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
 # webserver settings
@@ -14,3 +14,4 @@ WEBAPP_PORT = os.getenv('PORT', default=443)
 
 #HOST_URL = 'https://95.163.235.27'
 #WEBHOOK_HOST = f'https://api.telegram.org/bot5871071094:AAFxcN4mTaEfUlrQ_cRvmdougqygEj05uUI/setwebhook?url='+HOST_URL
+#95-163-235-27.cloudvps.regruhosting.ru
